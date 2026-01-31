0ad4ea368bcde8f795e6405ea76925d35a01f466
John Flinchbaugh <john@hjsoft.com>
2026-01-30 22:38:06 -0500

    connect to the server automatically

--- supernova.sh
+++ supernova.sh
@@ -6,7 +6,8 @@ supernova_pid=$!
 # Auto-connect supernova to system playback
 (
   sleep 2
-  if command -v jack_connect >/dev/null 2>&1; then
+  if command -v jack_connect >/dev/null 2>&1;
+then
     pw-jack jack_connect supernova:output_1 system:playback_1
     pw-jack jack_connect supernova:output_2 system:playback_2
     echo "Connected supernova to system playback."
@@ -15,7 +16,7 @@ supernova_pid=$!
   fi
 ) &
 
-# pw-jack clj -M:repl/rebel
-pw-jack clojure -Sdeps \{\:deps\ \{nrepl/nrepl\ \{\:mvn/version\ \"1.5.2\"\}\ cider/cider-nrepl\ \{\:mvn/version\ \"0.58.0\"\}\ refactor-nrepl/refactor-nrepl\ \{\:mvn/version\ \"3.11.0\"\}\}\ \:aliases\ \{\:cider/nrepl\ \{\:main-opts\ \[\"-m\"\ \"nrepl.cmdline\"\ \"--middleware\"\ \"\[refactor-nrepl.middleware/wrap-refactor\,cider.nrepl/cider-middleware\]\"\]\}\}\} -M:dev:cider/nrepl
+# Start clojure with necessary dependencies and run initialization before starting nREPL
+pw-jack clojure -Sdeps '{:deps {nrepl/nrepl {:mvn/version "1.5.2"} cider/cider-nrepl {:mvn/version "0.58.0"} refactor-nrepl/refactor-nrepl {:mvn/version "3.11.0"}}}' -M:dev -e "(do (require 'strudel-overtone.strudel-overtone) (strudel-overtone.strudel-overtone/-main) (require 'nrepl.cmdline) (nrepl.cmdline/-main \"--middleware\" \"[refactor-nrepl.middleware/wrap-refactor,cider.nrepl/cider-middleware]\"))"
 
 kill $supernova_pid
