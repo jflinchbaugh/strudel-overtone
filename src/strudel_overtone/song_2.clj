9ba6331802ba666e0de643440da412dcf2d19111
John Flinchbaugh <john@hjsoft.com>
2026-05-02 14:59:50 -0400

    playing around

--- src/strudel_overtone/song_2.clj
+++ src/strudel_overtone/song_2.clj
@@ -19,8 +19,36 @@
           (gain 0.5)
           (note [:- :a0])))
 
-  (ov/midi->hz (note->midi :g#2))
+  (play-only!
+    #_#_:kick (->
+            (s [[:kick :kick] :- [:kick :kick :kick :kick] :-])
+            (duck-trigger 1)
+            (sustain [2 0.15]))
+    #_#_:snare (->
+                 (s [:- :snare :- :snare]))
+    #_#_:bass (->
+                (s :fm)
+                (mono)
+                (glide 0.2)
+                (duck 1)
+                (note [:a0 :a0 :b0 :c1])))
+    (play!
+      :lead (->
+              (s :square)
+              (note (reverse (chord :a4 :minor)))
+              (detune (irand -50 50))
+              (ribbon 1 8)
+              (swing 0.2)
+              (fast 2)
+              (gain 0.6)))
 
-  (stop!)
+    (stop!)
 
-  .)
+    (scale :a4 :minor)
+    ;; => (69 71 72 74 76 77 79 81)
+
+    (chord :a4 :minor7)
+    ;; => (69 72 76 79)
+    ;; => (69 72 76)
+
+    .)
