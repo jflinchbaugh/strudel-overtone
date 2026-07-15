16644fb0df09b2d5ef8f2f6cb1eb75698ca3076f
John Flinchbaugh <john@hjsoft.com>
2026-07-14 23:47:48 -0400

    playing with degrees

--- src/strudel_overtone/degrees_demo.clj
+++ src/strudel_overtone/degrees_demo.clj
@@ -9,33 +9,42 @@
   (let [base-note :c2
         ;; A bassline walking through minor degrees
         bass (-> (note base-note)
-               (degrees :minor [1 1 4 5])
-               (s :sine)
-               (fast 2)
-               (gain 0.5)
-               (lpf 800)
-               (legato 0.8))
+                 (degrees :major [1 1 4 5])
+                 (s :saw)
+                 (fast 1/2)
+                 (gain 3/10)
+                 (lpf 100)
+                 (legato 0.8))
 
         ;; A lead melody using a slightly more complex degree pattern
         lead (-> (note base-note)
-               (add 12)
-               (degrees :major [1 3 4 5 4 3 7 8])
-               (s :supersaw)
-               (gain 0.8)
-               #_(lpf (sine 0.0525 500 4000))
-               (echo 0.375 0.6))
+                 (add 24)
+                 (s :supersaw)
+                 #_(swing 1/10)
+                 (degrees :major [1 3 4 5 7 6 7 8])
+                 (gain 5/5)
+                 (pan (sine 4/30 1 -1))
+                 (lpf (sine 1/60 400 4000))
+                 (echo 0.375 0.6))
 
         ;; Some drums to keep time
         drums (-> (s [:kick :- :- :-])
-                (gain 0.3)
-                (lpf (sine 0.0525 500 4000))
-                )]
+                  (gain 4/10)
+                  (slow 2)
+                  #_(lpf 400))
+
+        hat (-> (s [[:hat :hat] :- :hat :-])
+                  (gain 1/10)
+                  (legato 1/2)
+                  (slow 1)
+                  (lpf (sine 1/30 800 5000)))]
 
     (play-only!
-      :bass bass
-      :lead lead
-      :drums drums))
+     :bass bass
+     :lead lead
+     :drums drums
+     :hat hat))
 
   (stop!)
 
- .)
+  .)
